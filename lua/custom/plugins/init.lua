2d4314833f54b0932c202d7af2e5113959411cb4
pzzzyda <zhongyuan03@outlook.com>
2024-09-11 15:31:31 +0800

    Configured based on kickstart.

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,85 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        no_italic = true,
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = 'VeryLazy',
+    config = function()
+      vim.keymap.set('n', '[x', '<cmd>BufferLineCloseLeft', { desc = 'Close buffers on the Left' })
+      vim.keymap.set('n', ']x', '<cmd>BufferLineCloseRight', { desc = 'Close buffers one the Right' })
+      require('bufferline').setup {
+        options = {
+          diagnostics = 'nvim_lsp',
+          offsets = {
+            {
+              filetype = 'neo-tree',
+              text = 'File Explorer',
+              text_align = 'center',
+              separator = true,
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    'navarasu/onedark.nvim',
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        code_style = {
+          comments = 'none',
+          keywords = 'none',
+          functions = 'none',
+          strings = 'none',
+          variables = 'none',
+        },
+      }
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    keys = {
+      { '<leader>tt', '<cmd>ToggleTerm<CR>', desc = '[T]oggle [T]erminal' },
+    },
+    opts = {
+      direction = 'float',
+    },
+  },
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    keys = {
+      { '<leader>o', '<cmd>AerialToggle!<CR>', desc = '[O]utline' },
+    },
+    -- Optional dependencies
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
+  {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
+}
